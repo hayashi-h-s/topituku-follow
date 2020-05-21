bdadfbfeb0d1ba6e40a8c49bd796a62ffb89436a
haya <haya@appurunoMacBook-puro.local>
2020-05-21 20:08:00 +0900

    フォローフォロワー一覧の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
   before_action :user_signed_in?, only: %i[edit destroy]
   # before_action :correct_user,   only: [:edit, :update,:destroy]
@@ -9,5 +7,11 @@ class UsersController < ApplicationController
     @folders = @user.folders.all.page(params[:page]).per(6)
     @follow_count = @user.followings.count
     @follower_count = @user.followers.count
+  end
+
+  def follows
+    @user = User.find(params[:id])
+    @follow_list = @user.followings
+    @follower_list = @user.followers
   end
 end
